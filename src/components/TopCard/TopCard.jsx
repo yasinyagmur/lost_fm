aa9a3513e0e5b210c572aa97269bb66cf3b1e51a
yasin yağmur <yasin.yagmur@gmail.com>
2023-01-31 22:54:17 +0300

    the structure was established

---
+++ src/components/TopCard/TopCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopCard = () => {
+  return <div>TopCard</div>;
+};
+
+export default TopCard;
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Header/Navbar";
+import ArtistDetails from "../pages/ArtistDetails";
+import HomePage from "../pages/HomePage";
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/details" element={<ArtistDetails />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
