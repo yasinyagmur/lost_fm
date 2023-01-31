7c10736ca9d4f71db9445d253d86a4c3bb17bfa6
yasin yağmur <yasin.yagmur@gmail.com>
2023-02-01 00:43:56 +0300

    Navbar created and axios added

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import "./Navbar.css";
+import { TfiGithub, TfiLinkedin } from "react-icons/tfi";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <div className="wrapper">
+        <h2 className="header-title">Lost Fm</h2>
+        <ul className="header-navbar">
+          <li>
+            <TfiGithub /> Github
+          </li>
+          <li>
+            <TfiLinkedin /> Linkedin
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ListCard from "../components/ListCard/ListCard";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <ListCard />
+    </div>
+  );
 };
 
 export default HomePage;
